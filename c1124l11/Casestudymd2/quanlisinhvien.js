1654870a73fdab264d9305ccac226c5b12694ff0
NgThi Hanh <hahh78748@gmail.com>
2025-01-21 20:32:45 +0700

    Add buoi9

---
+++ c1124l11/Casestudymd2/quanlisinhvien.js
@@ -0,0 +1,17 @@
+
+function  save() {
+    const number = document.getElementById("number").value;
+    const fullname = document.getElementById("fullname").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const gioitinh = document.getElementById("gioitinh").value;
+    const diachi = document.getElementById("diachi").value;
+    const date = document.getElementById("date").value;
+
+    if (list - student)
+        if (number === '' || fullname === '' || email === '' || phone === '' || gioitinh === '' || diachi === '' || date === '') {
+            alert(' Vui lòng nhập đủ thông tin')
+            return;
+        }
+
+}
